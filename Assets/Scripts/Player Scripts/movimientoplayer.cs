e1ef37e19024607dde7a7e1df56564d944152fca
TeamBonziKong <team.bonzik@gmail.com>
2022-11-06 18:04:34 -0300

    feat: Actualizacion del mapa

---
+++ Assets/Scripts/Player Scripts/movimientoplayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoplayer : MonoBehaviour
+{
+    [SerializeField] private float valocidadMovimeinto;
+    [SerializeField] private Vector2 direccion;
+
+    private Rigidbody2D rb2D;
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    private void Update() 
+    {
+        direccion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;  
+    }
+
+    private void FixedUpdate() 
+    {
+        rb2D.MovePosition(rb2D.position + direccion * valocidadMovimeinto * Time.fixedDeltaTime);    
+    }
+}
